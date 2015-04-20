cfac49759d5d69e20a9b9a94ceb49cbe3742d1bd
Attila Suli <attila.suli@ponte.hu>
2015-04-20 18:24:44 +0200

    Finished basic datastructures excercises on lists.

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,65 +1,57 @@
 package fpinscala.datastructures
 
-sealed trait List[+A] // `List` data type, parameterized on a type, `A`
-case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
-case class Cons[+A](head: A, tail: List[A]) extends List[A] // Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`, which may be `Nil` or another `Cons`.
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
-object List { // `List` companion object. Contains functions for creating and working with lists.
-  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
-    case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
-  } 
-  
-  def product(ds: List[Double]): Double = ds match {
-    case Nil => 1.0
-    case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
-  }
-  
-  def apply[A](as: A*): List[A] = // Variadic function syntax
+object List {
+
+  def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42 
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101 
+  def append[A](left: List[A], right: List[A]): List[A] = foldRight(left, right)(Cons(_, _))
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = foldLeft(reverse(as), z)((b, a) => f(a, b))// Utility functions
+
+  def foldSum(ns: List[Int]) = foldRight(ns, 0)(_ + _)
+
+  def foldProduct(ns: List[Double]) = foldRight(ns, 1.0)(_ * _)
+
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, Nil) => Nil
+    case Cons(h, t) => t
   }
 
-  def append[A](a1: List[A], a2: List[A]): List[A] =
-    a1 match {
-      case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
+  def setHead[A](l: List[A], h: A): List[A] = Cons(h, tail(l))
+
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n - 1)
     }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
-    as match {
-      case Nil => z
-      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
-    }
-  
-  def sum2(ns: List[Int]) = 
-    foldRight(ns, 0)((x,y) => x + y)
-  
-  def product2(ns: List[Double]) = 
-    foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
+  def dropWhile[A](l: List[A], p: A => Boolean): List[A] = l match {
+    case Cons(h, t) if p(h) => dropWhile(t, p)
+    case _ => l
+  }
 
+  //not the most effective way to get the init of a list
+  def init[A](l: List[A]): List[A] = reverse(tail(reverse(l)))
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, acc) => acc + 1)
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h, t) => foldLeft(t, f(z, h))(f)
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  def reverse[A](l: List[A]): List[A] = foldLeft(l, Nil:List[A])((t, h) => Cons(h, t))
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  def flatMap[A,B](l: List[A])(f: A => List[B]) = foldRight(l, Nil:List[B])((x, xs) => append(f(x), xs))
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  def map[A,B](l: List[A])(f: A => B): List[B] = flatMap(l)(x => Cons(f(x), Nil))
 
-  def length[A](l: List[A]): Int = sys.error("todo")
-
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
-
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  def filter[A](l: List[A])(p: A => Boolean): List[A] = flatMap(l)(x => if (p(x)) Cons(x, Nil) else Nil)
 }
