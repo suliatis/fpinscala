8f58f524bfab3d74762ddba8fc25bd1cef7cc8f2
suliatis <suli.zakar.attila@gmail.com>
2015-04-21 22:47:18 +0200

    Finished excercises on Tree data structure.

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,15 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
+  def size[A](t: Tree[A]): Int = fold(t)(a => 1)(1 + _ + _)
 
+  def depth[A](t: Tree[A]): Int = fold(t)(a => 0)((d1,d2) => 1 + d1 max d2)
 
+  def map[A,B](t: Tree[A])(f: A => B) = fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_,_))
 
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+    case Leaf(x) => f(x)
+    case Branch(l,r) => g(fold(l)(f)(g),fold(r)(f)(g))
+  }
 
 }
