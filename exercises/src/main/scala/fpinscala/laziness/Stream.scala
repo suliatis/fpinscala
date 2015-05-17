a5a29b920d7bf912308e88b221947501e9445f26
suliatis <suli.zakar.attila@gmail.com>
2015-05-17 16:42:42 +0200

    Finish excersises of Laziness part.

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -3,29 +3,70 @@ package fpinscala.laziness
 import Stream._
 trait Stream[+A] {
 
-  def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h,t) => f(h(), t().foldRight(z)(f))
       case _ => z
     }
 
   def exists(p: A => Boolean): Boolean = 
-    foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
+    foldRight(false)((a, b) => p(a) || b)
 
   @annotation.tailrec
   final def find(f: A => Boolean): Option[A] = this match {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = sys.error("todo")
 
-  def drop(n: Int): Stream[A] = sys.error("todo")
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, t) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
 
-  def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => t().drop(n - 1)
+    case Cons(h, t) if n == 1 => t()
+    case _ => empty
+  }
 
-  def forAll(p: A => Boolean): Boolean = sys.error("todo")
+  def takeWhile(p: A => Boolean): Stream[A] = foldRight(Empty: Stream[A]){ (h, t) => if (p(h)) cons(h, t) else Empty }
 
-  def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
+  def toList: List[A] = this match {
+    case Cons(h, t) => h() :: t().toList
+    case _ => Nil
+  }
+
+  def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
+
+  def headOption: Option[A] = foldRight(None: Option[A]){ (h, _) => Some(h) }
+
+  def map[B](f: A => B): Stream[B] = foldRight(Empty: Stream[B])((h, t) => cons(f(h), t))
+
+  def map[B](f: A => B): Stream[B] = this match {
+    case Cons(h, t) =>
+  }
+
+  def filter(p: A => Boolean): Stream[A] = foldRight(Empty: Stream[A]) { (h, t) => if (p(h)) cons(h, t) else t }
+
+  def append[B>:A](s: Stream[B]): Stream[B] = foldRight(s)((h, t) => cons(h, t))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(Empty: Stream[B])((h, t) => f(h) append t)
+
+  def zipWith[B, C](s: Stream[B])(f: (A,B) => C): Stream[C] = unfold((this, s)) {
+    case (Cons(h1, t1), Cons(h2, t2)) => Some((f(h1(), h2()), (t1(), t2())))
+    case _ => None
+  }
+
+  def zip[B](s: Stream[B]): Stream[(A,B)] = zipWith(s)((_,_))
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] = zipWithAll(s2)((_,_))
+
+  def zipWithAll[B, C](s: Stream[B])(f: (Option[A], Option[B]) => C): Stream[C] = ???
+
+  def startsWith[B](s: Stream[B]): Boolean = ???
+
+  def tails: Stream[Stream[A]] = ???
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -43,8 +84,16 @@ object Stream {
     if (as.isEmpty) empty 
     else cons(as.head, apply(as.tail: _*))
 
-  val ones: Stream[Int] = Stream.cons(1, ones)
-  def from(n: Int): Stream[Int] = sys.error("todo")
+  lazy val ones: Stream[Int] = constants(1)
 
-  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = sys.error("todo")
+  def constants[A](c: A): Stream[A] = unfold(c)(s => Some((s, s)))
+
+  def from(n: Int): Stream[Int] = unfold(n)(s => Some((s, s + 1)))
+
+  def fibonacci: Stream[Int] = unfold((0, 1)) { case (a, b) => Some((a, (b, a + b))) }
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+    case Some((a, s)) => cons(a, unfold(s)(f))
+    case None => empty
+  }
 }
